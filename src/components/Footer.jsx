295c47a69c26d11c39480a68bd70f1d40aae5d44
David B <david.bolivarv4@gmail.com>
2023-09-15 12:56:58 -0500

    Finalización Desarrollo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+
+const Footer = () => {
+  return (
+    <footer>
+      <section className="foot-logo">
+        <p>Powered by</p>
+        <img src="../../public/images/DH.png" alt='DH-logo' />
+      </section>
+      <section className="icon">
+        <img src="../../public/images/ico-facebook.png" alt='DH-logo' />
+        <img src="../../public/images/ico-instagram.png" alt='DH-logo' />
+        <img src="../../public/images/ico-whatsapp.png" alt='DH-logo' />
+        <img src="../../public/images/ico-tiktok.png" alt='DH-logo' />
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
