83d58a0beeb9719612a1e72c07cd5c6e37ff3ead
David B <david.bolivarv4@gmail.com>
2023-09-15 14:05:38 -0500

    Fix url img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ const Footer = () => {
     <footer>
       <section className="foot-logo">
         <p>Powered by</p>
-        <img src="../../public/images/DH.png" alt='DH-logo' />
+        <img src="https://res.cloudinary.com/david-b/image/upload/v1694804463/DH/FrontIII/DH_af2m73.png" alt='DH-logo' />
       </section>
       <section className="icon">
-        <img src="../../public/images/ico-facebook.png" alt='DH-logo' />
-        <img src="../../public/images/ico-instagram.png" alt='DH-logo' />
-        <img src="../../public/images/ico-whatsapp.png" alt='DH-logo' />
-        <img src="../../public/images/ico-tiktok.png" alt='DH-logo' />
+        <img src="https://res.cloudinary.com/david-b/image/upload/v1694804464/DH/FrontIII/ico-facebook_tjvvvz.png" alt='fb-logo' />
+        <img src="https://res.cloudinary.com/david-b/image/upload/v1694804464/DH/FrontIII/ico-instagram_jq31vd.png" alt='insta-logo' />
+        <img src="https://res.cloudinary.com/david-b/image/upload/v1694804466/DH/FrontIII/ico-whatsapp_ggmxfw.png" alt='wa-logo' />
+        <img src="https://res.cloudinary.com/david-b/image/upload/v1694804465/DH/FrontIII/ico-tiktok_x1deqt.png" alt='tt-logo' />
       </section>
     </footer>
   )
